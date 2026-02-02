e50f5e1c13bc3728a5a793ae446e144bda4f9238
DOCHUNGHIEU8386 <quangngochieu266@gmail.com>
2026-02-02 22:16:02 +0700

    Update B3.java

--- B3.java
+++ B3.java
@@ -1,23 +1,16 @@
-class ScoreUtils {
+public class ScoreUtils {
 
-    // Kiem tra qua mon
-    public static boolean isPass(double score) {
+    // Kiểm tra Đạt / Trượt
+    public static boolean checkPass(double score) {
         return score >= 5.0;
     }
 
-    // Tinh diem trung binh
-    public static double calculateAverage(double s1, double s2, double s3) {
-        return (s1 + s2 + s3) / 3;
-    }
-}
-
-public class B3 {
-    public static void main(String[] args) {
-
-        double avg = ScoreUtils.calculateAverage(6.5, 7.0, 8.0);
-        System.out.println("Diem trung binh = " + avg);
-
-        boolean pass = ScoreUtils.isPass(avg);
-        System.out.println("Qua mon = " + pass);
+    // Tính điểm trung bình
+    public static double calculateAverage(double[] scores) {
+        double sum = 0;
+        for (double s : scores) {
+            sum += s;
+        }
+        return sum / scores.length;
     }
 }
