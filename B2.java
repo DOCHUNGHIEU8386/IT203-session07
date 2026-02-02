c4d83fb3668b0bb9708d37a0dbf509911db8163a
DOCHUNGHIEU8386 <quangngochieu266@gmail.com>
2026-02-02 22:13:21 +0700

    Update B2.java

--- B2.java
+++ B2.java
@@ -1,35 +1,33 @@
-class Nguoi {
-    String hoTen;
+public class Main {
 
-    public Nguoi(String hoTen) {
-        this.hoTen = hoTen;
+    // ===== Class Student (biến tham chiếu) =====
+    static class Student {
+        String name;
+
+        Student(String name) {
+            this.name = name;
+        }
     }
-}
 
-public class Bai2_Primitive_Reference {
     public static void main(String[] args) {
 
-        // ===== KIỂU PRIMITIVE =====
-        int x = 15;
-        int y = x; // sao chép GIÁ TRỊ
+        // ===== BIẾN NGUYÊN THỦY =====
+        int a = 10;
+        int b = a;   // sao chép GIÁ TRỊ
+        b = 20;
 
-        y = 30;
+        System.out.println("=== BIEN NGUYEN THUY ===");
+        System.out.println("a = " + a); // 10
+        System.out.println("b = " + b); // 20
 
-        System.out.println("=== PRIMITIVE ===");
-        System.out.println("x = " + x); // 15
-        System.out.println("y = " + y); // 30
-        // Primitive: biến độc lập, không ảnh hưởng lẫn nhau
+        // ===== BIẾN THAM CHIẾU =====
+        Student s1 = new Student("An");
+        Student s2 = s1; // sao chép ĐỊA CHỈ
 
+        s2.name = "Binh";
 
-        // ===== KIỂU REFERENCE =====
-        Nguoi n1 = new Nguoi("Do Chung Hieu");
-        Nguoi n2 = n1; // sao chép ĐỊA CHỈ
-
-        n2.hoTen = "B24DTCN336";
-
-        System.out.println("\n=== REFERENCE ===");
-        System.out.println("Nguoi 1 : " + n1.hoTen); // B24DTCN336
-        System.out.println("Nguoi 2 : " + n2.hoTen); // B24DTCN336
-        // Reference: nhiều biến cùng trỏ đến 1 object
+        System.out.println("\n=== BIEN THAM CHIEU ===");
+        System.out.println("s1.name = " + s1.name); // Binh
+        System.out.println("s2.name = " + s2.name); // Binh
     }
 }
