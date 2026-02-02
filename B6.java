96fe11efbb8da59e1d46f5170e929aaa40ca230e
DOCHUNGHIEU8386 <quangngochieu266@gmail.com>
2026-02-02 22:18:09 +0700

    Update B6.java

--- B6.java
+++ B6.java
@@ -1,83 +1,69 @@
-class User {
-    private final int id;   // final: khong the thay doi sau khi tao
+import java.util.ArrayList;
 
-    private String username;
-    private String password;
+public class Main {
 
-    public User(int id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
+    // ===== MODEL: User =====
+    static class User {
+        public final int id;        // ID bat bien (final)
+        public String username;
+        public String password;
 
-    public int getId() {
-        return id;
-    }
+        public User(int id, String username, String password) {
+            this.id = id;           // final bat buoc gan trong constructor
+            this.username = username;
+            this.password = password;
+        }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public boolean checkPassword(String password) {
-        return this.password.equalsIgnoreCase(password);
-    }
-
-    @Override
-    public String toString() {
-        return "User[id=" + id + ", username=" + username + "]";
-    }
-}
-
-class UserManager {
-    static User[] users = new User[10];
-    static int count = 0;
-
-    public static void addUser(User user) {
-        if (count < users.length) {
-            users[count] = user;
-            count++;
-            System.out.println("Them thanh cong user: " + user.getUsername());
-        } else {
-            System.out.println("Danh sach user da day");
+        @Override
+        public String toString() {
+            return "User[id=" + id + ", name=" + username + "]";
         }
     }
 
-    public static boolean checkLogin(String username, String password) {
-        for (int i = 0; i < count; i++) {
-            if (users[i].getUsername().equalsIgnoreCase(username)
-                    && users[i].checkPassword(password)) {
-                return true;
+    // ===== CONTROLLER: UserManager =====
+    static class UserManager {
+
+        // Danh sach user dung chung cho toan he thong
+        public static ArrayList<User> users = new ArrayList<>();
+
+        public static void addUser(User u) {
+            users.add(u);
+        }
+
+        public static boolean checkLogin(String username, String password) {
+            for (User u : users) {
+                if (u.username.equals(username) &&
+                    u.password.equals(password)) {
+                    return true;
+                }
             }
-        }
-        return false;
-    }
-
-    public static void showAllUsers() {
-        System.out.println("Danh sach nguoi dung:");
-        for (int i = 0; i < count; i++) {
-            System.out.println((i + 1) + ". " + users[i]);
+            return false;
         }
     }
-}
 
-public class B6 {
+    // ===== MAIN: Kiem thu he thong =====
     public static void main(String[] args) {
 
-        User u1 = new User(1, "hieu", "123456");
-        User u2 = new User(2, "admin", "admin123");
-        User u3 = new User(3, "student", "sv2024");
+        User u1 = new User(1, "dev_a", "123456");
+        User u2 = new User(2, "dev_b", "abcdef");
+        User u3 = new User(3, "dev_c", "999999");
 
         UserManager.addUser(u1);
         UserManager.addUser(u2);
         UserManager.addUser(u3);
 
-        System.out.println("\nDa them 3 user");
-        UserManager.showAllUsers();
+        System.out.println(">> Da them 3 user vao he thong.");
+        System.out.println("Danh sach hien tai:");
+        int index = 1;
+        for (User u : UserManager.users) {
+            System.out.println(index++ + ". " + u);
+        }
 
-        System.out.println("\nKiem tra dang nhap:");
-        System.out.println("Login (hieu, 123456): "
-                + (UserManager.checkLogin("hieu", "123456") ? "Thanh cong" : "That bai"));
-        System.out.println("Login (student, 123): "
-                + (UserManager.checkLogin("student", "123") ? "Thanh cong" : "That bai"));
-    }
-}
+        System.out.println("\n>> Kiem tra dang nhap:");
+        System.out.println("- Login (\"dev_a\", \"123456\"): "
+                + (UserManager.checkLogin("dev_a", "123456")
+                ? "Thanh cong!" : "That bai!"));
+
+        System.out.println("- Login (\"dev_b\", \"sai_pass\"): "
+                + (UserManager.checkLogin("dev_b", "sai_pass")
+                ? "Thanh con
